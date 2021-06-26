fe6a802b397b463d338e13ac24021d6917dd54ca
Awtanabe <panser098@gmail.com>
2021-06-26 12:00:45 +0900

    fix test

--- spec/models/todo_spec.rb
+++ spec/models/todo_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Todo, type: :model do
   describe '基本' do
-    todo = Todo.new(title: nil)
+    todo = Todo.new(title: "title")
     todo.valid?
     it { expect(todo).to be_valid }
   end
